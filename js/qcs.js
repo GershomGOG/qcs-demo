ed497e630ab8abed542a12e13c127e011f8000df
GershomGOG <gg2152@LPT-006928-AA.local>
2016-06-11 10:38:25 -0400

    Creating js files

---
+++ js/qcs.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+
+function doorWay(x,y)
+{
+
+
+document.getElementById(x).style.display = 'block'; document.getElementById(y).style.display = 'none' ;
+console.info('door used');
+}
